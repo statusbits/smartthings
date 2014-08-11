d81c79b3ff7279d7198e14b08d17740250f8ce26
geko <geko@statusbits.com>
2014-08-10 22:18:50 -0700

    Added Virtual Contact Tile SmartDevice

--- VirtualThings/README.md
+++ VirtualThings/README.md
@@ -12,7 +12,7 @@ implements "Switch" device
 fire "on" and "off" events when a user touches the switch tile.
 
 
-### Virtual 8-button Remote Control
+### 8-button Virtual Remote Control
 
     File: VirtualRemoteControl.device.groovy
 
@@ -28,14 +28,27 @@ home "modes", trigger "Hello Home" actions, etc. Note, that Button
 Controller app supports only four buttons though.
 
 
+### Virtual Contact Tile
+
+    File: VirtualContactTile.device.groovy
+
+This virtual device implements SmartThings "Contact Sensor" device capability
+and can be used to display status of an open/closed contact from external
+source. The device current status can be set by calling its 'setCurrentValue'
+command with "open" or "closed" string as an argument:
+
+    setCurrentValue("open")
+
+
 ### Virtual Temperature Tile
 
     File: VirtualTemperatureTile.device.groovy
 
-This virtual device implements SmartThings "Temperature" device capability
-and can be used to display temperature data from external source. The device
-current temperature value can be set by calling its 'setCurrentValue'
-command with temperature value (in degrees of Fahrenheit) as an argument:
+This virtual device implements SmartThings "Temperature Measurement" device
+capability and can be used to display temperature data from external source.
+The device current temperature value can be set by calling its
+'setCurrentValue' command with temperature value (in degrees of Fahrenheit)
+as an argument:
 
     setCurrentValue(72.5)
 
---
+++ VirtualThings/VirtualContactTile.device.groovy
@@ -0,0 +1,77 @@
+/**
+ *  Virtual Contact Tile
+ *
+ *  Copyright (c) 2014 Statusbits.com
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may
+ *  not use this file except in compliance with the License. You may obtain a
+ *  copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ *  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ *  License  for the specific language governing permissions and limitations
+ *  under the License.
+ *
+ *  The latest version of this file can be found at:
+ *  https://github.com/statusbits/smartthings/blob/master/VirtualThings/VirtualContactTile.device.groovy
+ *
+ *  Version: 1.0.0
+ *  Date: 2014-08-10
+ */
+
+metadata {
+    definition (name:"Virtual Contact Tile", namespace:"statusbits", author:"geko@statusbits.com") {
+        capability "Contact Sensor"
+        capability "Sensor"
+
+        // custom commands
+        command "setCurrentValue"
+    }
+
+    tiles {
+        standardTile("contact", "device.contact", width: 2, height: 2) {
+            state "open", label: '${name}', icon: "st.contact.contact.open", backgroundColor: "#ffa81e"
+            state "closed", label: '${name}', icon: "st.contact.contact.closed", backgroundColor: "#79b821"
+        }
+
+        main(["contact"])
+        details(["contact"])
+    }
+
+    simulator {
+        status "open"   : "current_value:open"
+        status "closed" : "current_value:closed"
+        status "Invalid message" : "foobar"
+    }
+}
+
+def parse(String message) {
+    TRACE("parse(${message})")
+
+    def msg = stringToMap(message)
+    if (msg.current_value) {
+        setCurrentValue(msg.current_value)
+    } else {
+        log.error "Invalid message: ${message}"
+    }
+
+    return null
+}
+
+def setCurrentValue(value) {
+    TRACE("setCurrentValue(${value})")
+
+    def event = [
+        name  : "contact",
+        value : value,
+    ]
+
+    sendEvent(event)
+}
+
+private def TRACE(message) {
+    log.debug message
+}
