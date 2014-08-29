33147c6adf648c133b367aed621d7de263096579
geko <geko@statusbits.com>
2014-08-28 18:32:11 -0700

    X10 Bridge - work in progress

--- X10Bridge/ActiveEyeBridge.app.groovy
+++ X10Bridge/ActiveEyeBridge.app.groovy
@@ -27,8 +27,9 @@
  *  - ActiveEye product page: http://www.x10.com/ms16a.html
  *  - ActiveEye setup instructions: http://kbase.x10.com/wiki/Active_Eye_Motion_Sensor_Setup
  *
- *  Version: 0.9.0
- *  Date: 2014-08-18
+ *  Revision History
+ *  ----------------
+ *  2014-08-18  V0.9.0  Initial check-in
  */
 
 definition(
@@ -44,194 +45,163 @@ definition(
 
 preferences {
     page name:"setupInit"
-    page name:"setupWizard", title:"Setup Wizard", nextPage:"setupWizard", install:false
-    page name:"setupAddDevice", nextPage:"setupWizard", install:false
+    page name:"setupMenu", title:"Main Menu", nextPage:null
+    page name:"setupAddDevice"
+    page name:"actionAddDevice"
+    page name:"setupRemoveDevice"
+    page name:"actionRemoveDevice"
 }
 
 private def setupInit() {
     TRACE("setupInit()")
-    log.debug "app: ${app.getProperties()}"
 
-    if (!state.setup) {
-        // Initialize setup state
-        state.setup = [:]
-        state.setup.menu = true
-        state.setup.installed = false
-        state.setup.devices = [:]
-    } else {
-        // Start in menu mode
-        state.setup.menu = true
-
-        if (state.setup.installed) {
-            // Skip welcome page
-            return setupWizard()
-        }
+    if (state.setup) {
+        // already initialized, go to setup menu
+        return setupMenu()
     }
 
-    return pageWelcome()
+    // initialize setup state and show welcome page
+    state.setup = [:]
+    state.setup.installed = false
+    state.setup.devices = [:]
+    return setupWelcome()
 }
 
-// Setup wizard state machine.
-def setupWizard() {
-    TRACE("setupWizard()")
+// Show setup welcome page
+private def setupWelcome() {
+    TRACE("setupWelcome()")
 
-    if (state.setup.devices.size() == 0) {
-        // Jump to the "Add Device" page 
-        return pageAddDevice()
-    }
-
-    if (state.setup.menu) {
-        // Jump to the 'Main Menu' page
-        return pageMainMenu()
-    }
-
-    // Jump to appropriate menu action page
-    def nextPage
-    switch (settings.setupAction) {
-        case "Add Sensors":
-            nextPage = pageAddDevice()
-            break
-
-        case "Remove Sensors":
-            nextPage = setupRemoveDevice()
-            break
-
-        case "List Sensors":
-            nextPage = setupListDevices()
-            break
-
-        case "Exit Setup":
-            nextPage = setupFinish()
-            break
-
-        default:
-            nextPage = pageMainMenu()
-    }
-
-    state.setup.menu = true
-    return nextPage
-}
-
-// Generates "Welcome" page
-private def pageWelcome() {
-    TRACE("pageWelcome()")
-
-    def textWelcome =
-        "ActiveEye Bridge integrates X10 ActiveEye (Model MS16A) and/or " +
-        "EagleEye (Model MS14A) motion sensors into SmartThings.\n\n" +
+    def textPara1 =
+        "ActiveEye Bridge integrates X10 ActiveEye (model MS16A) and/or " +
+        "EagleEye (model MS14A) motion sensors into SmartThings.\n\n" +
         "ActiveEye Bridge does not rely on the SmartThings Hub to receive " +
         "signals from the motion sensors. Instead, it requires an X10 " +
-        "serial Firecracker receiver (Model MR26A) and an ActiveEye Gateway " +
-        "software installed on a PC. Please visit [www.statusbits.com/p/activeeye] " +
-        "for more information.\n\n" +
-        "Please read the Licence Agreement below, then tap the 'Next' button " +
-        "at the top of the screen to accept the terms and conditions of the " +
-        "License Agreement and continue."
+        "serial Firecracker receiver (model MR26A) and an X10 Gateway " +
+        "software installed on a PC. Please visit " +
+        "[www.statusbits.com/p/activeeye] for more information."
+
+    def textPara2 = "${app.name}. ${textVersion()}\n${textCopyright()}"
+
+    def textPara3 =
+        "Please read the License Agreement below, then tap the 'Next' " +
+        "button at the top of the screen to accept the terms and " +
+        "conditions of the License Agreement and continue."
 
     def textLicense =
         "Licensed under the Apache License, Version 2.0 (the \"License\"); " +
-        "you may not use this software except in compliance with the License. " +
-        "You may obtain a copy of the License at:\n\n" +
+        "you may not use this software except in compliance with the " +
+        "License. You may obtain a copy of the License at:\n\n" +
         "http://www.apache.org/licenses/LICENSE-2.0\n\n" +
-        "Unless required by applicable law or agreed to in writing, software " +
-        "distributed under the License is distributed on an \"AS IS\" BASIS, " +
-        "WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or " +
-        "implied. See the License for the specific language governing " +
-        "permissions and limitations under the License."
+        "Unless required by applicable law or agreed to in writing, " +
+        "software distributed under the License is distributed on an " +
+        "\"AS IS\" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, " +
+        "either express or implied. See the License for the specific " +
+        "language governing permissions and limitations under the License."
 
     def pageProperties = [
         name:       "setupInit",
         title:      "Welcome!",
-        nextPage:   "setupWizard",
-        uninstall:  false
+        nextPage:   "setupMenu",
+        install:    false,
+        uninstall:  state.setup.installed
     ]
 
     return dynamicPage(pageProperties) {
         section {
-            paragraph textWelcome
+            paragraph textPara1
+            paragraph textPara2
+            paragraph textPara3
         }
         section("License") {
             paragraph textLicense
         }
-        section("Copyright (c) 2014 Statusbits.com") {}
     }
 }
 
-// Generates "Main Menu" page
-private def pageMainMenu() {
-    TRACE("pageMainMenu()")
+// Show "Main Menu" page
+private def setupMenu() {
+    TRACE("setupMenu()")
 
-    // Setup action menu
-    def actions = [
-        "Add Sensors",
-        "Remove Sensors",
-        "List Sensors",
-        "Exit Setup"
-    ]
+    if (state.setup.devices.size() == 0) {
+        // Jump to the "Add Device" page 
+        //return setupAddDevice()
+    }
 
     def text =
         "Tap the Main Menu below and select one of the options, then tap the " +
         "'Next' button at the top of the screen to continue."
 
+    def pageProperties = [
+        name:       "setupMenu",
+        //title:      "Setup Wizard",
+        //nextPage:   "setupWizard",
+        install:    true,
+        uninstall:  state.setup.installed
+    ]
+
     state.setup.menu = false
-    return dynamicPage(name:"setupWizard", uninstall:state.setup.installed) {
+    return dynamicPage(pageProperties) {
         section("Main Menu") {
             paragraph text
-            input name: "setupAction", title:"Main Menu", type: "enum", required:true,
-                description: "Tap to select option", metadata:[values:actions]
+            //input name: "setupAction", title:"Main Menu", type: "enum", required:true,
+            //    description: "Tap to select option", metadata:[values:actions]
+
+            href "setupAddDevice", title:"Add Motion Sensor", description:"Tap to open"
         }
     }
 }
 
-// Generates "Add Device" page
-private def pageAddDevice() {
-    TRACE("pageAddDevice()")
+// Show "Add Device" setup page
+private def setupAddDevice() {
+    TRACE("setupAddDevice()")
+
+    def helpName =
+        "Give the motion sensor a descriptive name, for example 'Hallway Motion'."
+
+    def helpAddress =
+        "Each ActiveEye motion sensor is assigned a unique X10 address, " +
+        "consisting of the 'House Code' (letters A through P) and the " +
+        "'Unit Code' (numbers 1 through 16). Please enter the sensor's " +
+        "X10 address below."
+
+    def helpContinue =
+        "Select 'Yes' if you wish to add more sensors, then tap the 'Next' " +
+        "button at the top of the screen to continue."
 
     def houseCodes = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P"]
     def unitCodes = ["1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16"]
-    def helpName =
-        "Give the motion sensor a descriptive name, for example 'Hallway Motion'."
-    def helpAddress =
-        "Each ActiveEye motion sensor is assigned a unique X10 address, " +
-        "consisting of the 'House Code' (letters A through P) and the 'Unit Code' " +
-        "(numbers 1 through 16). Please enter the sensor's X10 address below."
-    def helpContinue =
-        "Select 'Yes' if you wish to add more sensors, then tap the 'Next' button " +
-        "at the top of the screen to continue."
 
-    return dynamicPage(name:"setupWizard", title:"Add Motion Sensor", nextPage:"setupAddDevice", uninstall:state.setup.installed) {
+    def pageProperties = [
+        name:       "setupWizard",
+        title:      "Add Motion Sensor",
+        nextPage:   "actionAddDevice",
+        uninstall:  state.setup.installed
+    ]
+
+    return dynamicPage(pageProperties) {
         section("Sensor Name") {
-            input "setupDevName", "string", title: "What is your sensor name?", required:true
             paragraph helpName
+            input "setupDevName", "string", title: "What is your sensor name?", required:true
         }
         section("X10 Address") {
+            paragraph helpAddress
             input "setupHouseCode", "enum", title: "What is your House Code?",
                 metadata:[values:houseCodes], required:true
             input "setupUnitCode", "enum", title: "What is your Unit Code?",
                 metadata:[values:unitCodes], required:true
-            paragraph helpAddress
-        }
-        section("Continue") {
-            input "setupAddMore", "enum", title: "Add more sensors?",
-                metadata:[values:["Yes","No"]], required:true
-            paragraph helpContinue
         }
     }
 }
 
-private def setupAddDevice() {
-    TRACE("setupAddDevice()")
+private def actionAddDevice() {
+    TRACE("actionAddDevice()")
 
     def devAddr = settings.setupHouseCode + settings.setupUnitCode
     addDevice(devAddr, settings.setupDevName)
-
-    if (settings.setupAddMore == 'Yes') {
-        return pageAddDevice()
-    } else {
-        return pageMainMenu()
-    }
+    return setupMenu()
 }
 
+// Show "Remove Device" setup page
 private def setupRemoveDevice() {
     TRACE("setupRemoveDevice()")
 
@@ -240,6 +210,13 @@ private def setupRemoveDevice() {
             paragraph "Tap 'Next' to continue."
         }
     }
+}
+
+private def actionRemoveDevice() {
+    TRACE("actionRemoveDevice()")
+
+    //removeDevice(devAddr, settings.setupDevName)
+    return setupMenu()
 }
 
 private def setupListDevices() {
@@ -313,7 +290,7 @@ def updated() {
 
 def initialize() {
     TRACE("initialize()")
-    log.trace "${app.name}. Version 0.1.0. Copyright (c) 2014, Statusbits.com"
+    log.trace "${app.name}. ${textVersion()}. ${textCopyright()}"
 
     // subscribe to attributes, devices, locations, etc.
     subscribe(app, onAppTouch)
@@ -337,15 +314,16 @@ def onAppTouch(evt) {
     TRACE("onAppTouch(${evt})")
 }
 
-private def textAbout() {
-    def text =
-        "Version 0.1.0\n" +
-        "Copyright (c) 2014 Statusbits.com\n\n" +
-        "Visit [www.statusbits.com/p/activeeye] for more information."
+private def textVersion() {
+    def text = "Version 0.9.0"
+}
+
+private def textCopyright() {
+    def text = "Copyright (c) 2014 Statusbits.com"
 }
 
 private def TRACE(message) {
     log.debug message
-    log.debug "settings: ${settings}"
     log.debug "state: ${state}"
+    log.debug "settings: ${settings}"
 }
