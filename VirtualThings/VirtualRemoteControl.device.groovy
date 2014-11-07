afc42ae5b6231b4c11d7247c47e1c894fefcd8b9
geko <geko@statusbits.com>
2014-11-07 02:18:09 -0800

    Virtual Remote - added configurable button labels.

--- VirtualThings/VirtualRemoteControl.device.groovy
+++ VirtualThings/VirtualRemoteControl.device.groovy
@@ -1,6 +1,8 @@
 /**
  *  Virtual Remote Control
  *
+ *  --------------------------------------------------------------------------
+ *
  *  Copyright (c) 2014 Statusbits.com
  *
  *  Licensed under the Apache License, Version 2.0 (the "License"); you may
@@ -15,11 +17,12 @@
  *  License  for the specific language governing permissions and limitations
  *  under the License.
  *
+ *  --------------------------------------------------------------------------
+ *
  *  The latest version of this file can be found at:
  *  https://github.com/statusbits/smartthings/blob/master/VirtualThings/VirtualRemoteControl.device.groovy
  *
- *  Version: 1.0.0
- *  Date: 2014-08-07
+ *  Version: 1.1.0  (2014-11-07)
  */
 
 metadata {
@@ -39,48 +42,64 @@ metadata {
         command "button8"
 
         // Custom attributes
-        //attribute "label1", "string"
+        attribute "deviceName", "string"
+        attribute "label1", "string"
+        attribute "label2", "string"
+        attribute "label3", "string"
+        attribute "label4", "string"
+        attribute "label5", "string"
+        attribute "label6", "string"
+        attribute "label7", "string"
+        attribute "label8", "string"
     }
 
     preferences {
-        //input("label_1", "string", title:"Button 1 Label", required:false, displayDuringSetup:false)
+        input("label1", "string", title:"Button 1 Label", defaultValue:"Button 1", required:false, displayDuringSetup:false)
+        input("label2", "string", title:"Button 2 Label", defaultValue:"Button 2", required:false, displayDuringSetup:false)
+        input("label3", "string", title:"Button 3 Label", defaultValue:"Button 3", required:false, displayDuringSetup:false)
+        input("label4", "string", title:"Button 4 Label", defaultValue:"Button 4", required:false, displayDuringSetup:false)
+        input("label5", "string", title:"Button 5 Label", defaultValue:"Button 5", required:false, displayDuringSetup:false)
+        input("label6", "string", title:"Button 6 Label", defaultValue:"Button 6", required:false, displayDuringSetup:false)
+        input("label7", "string", title:"Button 7 Label", defaultValue:"Button 7", required:false, displayDuringSetup:false)
+        input("label8", "string", title:"Button 8 Label", defaultValue:"Button 8", required:false, displayDuringSetup:false)
     }
 
     tiles {
-        standardTile("remote", "device.button", canChangeIcon:true, canChangeBackground:true) {
-            state "default", label:"", icon:"st.unknown.zwave.remote-controller"
+        //standardTile("remote", "device.deviceName", canChangeIcon:true, canChangeBackground:true) {
+        standardTile("remote", "device.deviceName") {
+            state "default", label:'${currentValue}', icon:"st.unknown.zwave.remote-controller"
         }
 
-        standardTile("button1", "device.button", inactiveLabel:false, decoration:"flat") {
-            state "default", label:"Button 1", icon:"st.unknown.thing.thing-circle", action:"button1"
+        standardTile("button1", "device.label1", inactiveLabel:false, decoration:"flat") {
+            state "default", label:'${currentValue}', icon:"st.unknown.thing.thing-circle", action:"button1"
         }
 
-        standardTile("button2", "device.button", inactiveLabel:false, decoration:"flat") {
-            state "default", label:"Button 2", icon:"st.unknown.thing.thing-circle", action:"button2"
+        standardTile("button2", "device.label2", inactiveLabel:false, decoration:"flat") {
+            state "default", label:'${currentValue}', icon:"st.unknown.thing.thing-circle", action:"button2"
         }
 
-        standardTile("button3", "device.button", inactiveLabel:false, decoration:"flat") {
-            state "default", label:"Button 3", icon:"st.unknown.thing.thing-circle", action:"button3"
+        standardTile("button3", "device.label3", inactiveLabel:false, decoration:"flat") {
+            state "default", label:'${currentValue}', icon:"st.unknown.thing.thing-circle", action:"button3"
         }
 
-        standardTile("button4", "device.button", inactiveLabel:false, decoration:"flat") {
-            state "default", label:"Button 4", icon:"st.unknown.thing.thing-circle", action:"button4"
+        standardTile("button4", "device.label4", inactiveLabel:false, decoration:"flat") {
+            state "default", label:'${currentValue}', icon:"st.unknown.thing.thing-circle", action:"button4"
         }
 
-        standardTile("button5", "device.button", inactiveLabel:false, decoration:"flat") {
-            state "default", label:"Button 5", icon:"st.unknown.thing.thing-circle", action:"button5"
+        standardTile("button5", "device.label5", inactiveLabel:false, decoration:"flat") {
+            state "default", label:'${currentValue}', icon:"st.unknown.thing.thing-circle", action:"button5"
         }
 
-        standardTile("button6", "device.button", inactiveLabel:false, decoration:"flat") {
-            state "default", label:"Button 6", icon:"st.unknown.thing.thing-circle", action:"button6"
+        standardTile("button6", "device.label6", inactiveLabel:false, decoration:"flat") {
+            state "default", label:'${currentValue}', icon:"st.unknown.thing.thing-circle", action:"button6"
         }
 
-        standardTile("button7", "device.button", inactiveLabel:false, decoration:"flat") {
-            state "default", label:"Button 7", icon:"st.unknown.thing.thing-circle", action:"button7"
+        standardTile("button7", "device.label7", inactiveLabel:false, decoration:"flat") {
+            state "default", label:'${currentValue}', icon:"st.unknown.thing.thing-circle", action:"button7"
         }
 
-        standardTile("button8", "device.button", inactiveLabel:false, decoration:"flat") {
-            state "default", label:"Button 8", icon:"st.unknown.thing.thing-circle", action:"button8"
+        standardTile("button8", "device.label8", inactiveLabel:false, decoration:"flat") {
+            state "default", label:'${currentValue}', icon:"st.unknown.thing.thing-circle", action:"button8"
         }
 
         standardTile("refresh", "device.button", inactiveLabel:false, decoration:"flat") {
@@ -95,10 +114,9 @@ metadata {
     }
 
     simulator {
-        status "Button 1": "button:1, action:pushed"
-        status "Button 2": "button:2, action:pushed"
-        status "Button 3": "button:3, action:pushed"
-        status "Button 4": "button:4, action:pushed"
+        for (int n = 1; n <= 8; n++) {
+            status "Button ${n}": "button:${n}, action:pushed"
+        }
     }
 }
 
@@ -117,6 +135,18 @@ def parse(String message) {
 
 def refresh() {
     TRACE("refresh()")
+
+    sendEvent(name: "deviceName", value: device.displayName)
+    sendEvent(name: "label1", value: settings.label1)
+    sendEvent(name: "label2", value: settings.label2)
+    sendEvent(name: "label3", value: settings.label3)
+    sendEvent(name: "label4", value: settings.label4)
+    sendEvent(name: "label5", value: settings.label5)
+    sendEvent(name: "label6", value: settings.label6)
+    sendEvent(name: "label7", value: settings.label7)
+    sendEvent(name: "label8", value: settings.label8)
+
+    STATE()
 }
 
 def button1()   { buttonPush(1) }
@@ -150,5 +180,19 @@ private def buttonEvent(button, action) {
 }
 
 private def TRACE(message) {
-    //log.debug message
+    log.debug message
+}
+
+private def STATE() {
+    log.debug "settings: ${settings}"
+    log.debug "Display Name: ${device.displayName}"
+    log.debug "Device Name: ${device.currentValue("deviceName")}"
+    log.debug "Label 1: ${device.currentValue("label1")}"
+    log.debug "Label 2: ${device.currentValue("label2")}"
+    log.debug "Label 3: ${device.currentValue("label3")}"
+    log.debug "Label 4: ${device.currentValue("label4")}"
+    log.debug "Label 5: ${device.currentValue("label5")}"
+    log.debug "Label 6: ${device.currentValue("label6")}"
+    log.debug "Label 7: ${device.currentValue("label7")}"
+    log.debug "Label 8: ${device.currentValue("label8")}"
 }
