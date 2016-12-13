6d359f3ef263d407d4a5687e3c9e54ed6ae94a56
geko <geko@statusbits.com>
2016-12-13 14:19:51 -0800

    RadioThermostat (wip).

--- devicetypes/statusbits/radio-thermostat.src/radio-thermostat.groovy
+++ devicetypes/statusbits/radio-thermostat.src/radio-thermostat.groovy
@@ -59,23 +59,17 @@ metadata {
     tiles(scale:2) {
         multiAttributeTile(name:"thermostat", type:"thermostat", width:6, height:4) {
 		    tileAttribute ("device.temperature", key:"PRIMARY_CONTROL") {
-			    attributeState "temperature", label:'${currentValue}°',
-				    backgroundColors:[
-					    [value:31, color:"#153591"],
-					    [value:44, color:"#1e9cbb"],
-					    [value:59, color:"#90d2a7"],
-					    [value:74, color:"#44b621"],
-					    [value:84, color:"#f1d801"],
-					    [value:95, color:"#d04e00"],
-					    [value:96, color:"#bc2323"]
-				    ]
+			    attributeState("default", label:'${currentValue}°', unit:"dF")
 	        }
 			tileAttribute("device.temperature", key:"VALUE_CONTROL") {
 				attributeState("VALUE_UP", action:"tempUp")
 				attributeState("VALUE_DOWN", action:"tempDown")
 			}
-			tileAttribute("device.humidity", key:"SECONDARY_CONTROL") {
-				attributeState("default", label:'${currentValue}%', unit:"%")
+			//tileAttribute("device.humidity", key:"SECONDARY_CONTROL") {
+			//	attributeState("default", label:'${currentValue}%', unit:"%")
+			//}
+			tileAttribute("device.fanState", key:"SECONDARY_CONTROL") {
+				attributeState("default", label:'Fan: ${currentValue}')
 			}
 			tileAttribute("device.thermostatOperatingState", key:"OPERATING_STATE") {
 				attributeState("idle", backgroundColor:"#44b621")
